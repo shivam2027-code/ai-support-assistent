32d27ff44800cbf018954532aa633f1ee76de951
Shivam Rajput <shivam2027.code@gmail.com>
2026-09-23 17:57:21 +0530

    allow production frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ai-support-agent.vercel.app"
         "https://ai-support-assistent-71xhihgzk-team-hydra3.vercel.app/"
     ],
     allow_credentials=True,
